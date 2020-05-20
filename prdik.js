75eae10f906c97802db846109a6c79d24e562bd4
Michal Zorkovsky <zorkovsky@gmail.com>
2020-05-20 20:15:01 +0200

    Dorkine vylevy

---
+++ prdik.js
@@ -0,0 +1,18 @@
+console.log('PRRRRD');
+
+var a=40;
+var b=123092148203591135;
+
+var sum = a*b;
+
+var e = "PRD";
+var s = 'Klarka je prdicek';
+var totentext = ('PRDOL SOM SI '+sum.toLocaleString('en-US')+' KRAT. ACH JAJ');
+
+var cislo = 1;
+
+console.log(e.toLowerCase());
+
+console.log(totentext.toLowerCase());
+console.log();
+console.log(s+sum+b+a);
